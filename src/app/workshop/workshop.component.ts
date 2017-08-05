7d58a9d006f6b58a633b74cd031a388883229c6a
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2017-08-05 15:58:34 +0430

    Implement selecting items to purchase

--- src/app/workshop/workshop.component.ts
+++ src/app/workshop/workshop.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-workshop',
-  templateUrl: './workshop.component.html',
-  styleUrls: ['./workshop.component.css']
+    selector: 'app-workshop',
+    templateUrl: './workshop.component.html',
+    styleUrls: ['./workshop.component.css']
 })
 export class WorkshopComponent implements OnInit {
+    soldItems = [false, false, false ,false];
+    prices = [25000, 10000, 75000, 20000];
+    totalPrice = 0;
+    signs = [1, 1, 1, 1];
 
-  constructor() { }
+    constructor() { }
+    
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
-
+    public togglePurchaseItem(itemId) {
+        this.soldItems[itemId] = !this.soldItems[itemId];
+        this.totalPrice += this.signs[itemId] * this.prices[itemId];
+        this.signs[itemId] *= -1;
+    }
+    
 }
