5f3325254c2ecd7636b3456683afef650949ed75
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2017-10-01 02:21:48 +0330

    Add authGuard for login routes

---
+++ src/app/_guard/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(
+        private router: Router
+    ) { }
+
+    canActivate() {
+        if (localStorage.getItem('CurrentUser')) {
+            return true;
+        }
+
+        this.router.navigate(['/login']);
+
+        return false;
+    }
+}
