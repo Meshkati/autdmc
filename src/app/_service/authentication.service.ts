ddaf5cd179418d3e844d2f7e927ba2520171f2e9
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2017-10-01 02:23:09 +0330

    Add authentication service

---
+++ src/app/_service/authentication.service.ts
@@ -0,0 +1,44 @@
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthenticationService {
+    private url = "/api";
+    token: string;
+
+    constructor(
+        private http: Http
+    ) { }
+
+    login(username: string, password: string):Observable<boolean> {
+        let headers = new Headers({'Content-Type': 'application/json'});
+        let options = new RequestOptions ({headers: headers});
+        const requestData = {
+            
+        }
+
+        return this.http.post(this.url + '/login', requestData, options)
+        .map((response: Response) => {
+            let token = response.json() && response.json().token
+            if (token) {
+                this.token = token
+                const currentUser = {
+                    username: username,
+                    token: token
+                }
+
+                localStorage.setItem('currentUser', JSON.stringify(currentUser))
+                return true
+            } else {
+                return false
+            }
+        });
+    }
+
+    private extractData(res: Response) {
+        let body = res.json()
+        return body;
+    }
+}
