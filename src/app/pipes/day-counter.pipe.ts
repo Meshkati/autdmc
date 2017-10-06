5d4de7446431ae98aa9508e4eacde50c29c108b9
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2017-10-06 18:43:51 +0330

    Add other time units to day-counter pipe

--- src/app/pipes/day-counter.pipe.ts
+++ src/app/pipes/day-counter.pipe.ts
@@ -1,16 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'dayCounter'
+    name: 'dayCounter',
+    pure: false
 })
 
 export class DayCounterPipe implements PipeTransform {
     transform(value: any, ...args: any[]): any {
-        const day = new Date(value)
-        const today = new Date()
-        const dif = (day.getTime() - today.getTime()) / 86400000;
+        const day = new Date(value).getTime()
+        const now = new Date().getTime()
+        const dif = day - now
+        
+        const dayDif = dif / 86400000;
+        const hourDif = (dif / 3600000) % 24;
+        const minDif = (dif / 60000) % 60;
+        const secDif = (dif / 1000) % 60;
         
         
-        return dif.toFixed() + ' روز '        
+        return dayDif.toFixed() + ' روز '
+            + hourDif.toFixed() + ' ساعت '
+            + minDif.toFixed() + ' دقیقه '
+            + secDif.toFixed() + ' ثانیه '
     }
 }
