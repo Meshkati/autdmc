8d7050d0896d73db7bcb7f4777dcbdd9fb721362
Ahmad Anvari <ahmad.anv@gmail.com>
2018-11-23 12:13:14 +0330

    Add config file

---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    PORT: process.env.PORT || '3000',
+
+    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_PORT: process.env.DB_HOST || '27017',
+    DB_NAME: process.env.DB_NAME || 'autdmc'
+};
