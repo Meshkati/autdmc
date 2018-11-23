7735a5e686a4d538a49f8048270c1292e635e83e
Ahmad Anvari <ahmad.anv@gmail.com>
2018-11-23 23:15:16 +0330

    Add login and signup

--- server/routes/auth/login.js
+++ server/routes/auth/login.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const asyncMiddleware = require('../../middlewares/async');
+const bodyValidationMiddleware = require('../../middlewares/validation').bodyValidation;
+const validationSchema = require('../../validations/signup-teams').signupSchema;
 
-router.get('/', (req, res) => {
+const bcrypt = require('bcrypt');
+const generatePassword = require('../../util/passwd-gen').generatePassword;
+
+router.get('/', asyncMiddleware(async (req, res) => {
     res.json({b: 13})
-});
+}));
+
+
+
+router.post('/', bodyValidationMiddleware(validationSchema) ,asyncMiddleware(async (req, res) => {
+
+
+
+    let hash = await bcrypt.hash('somepassword', 10);
+
+    let cmp1 = await bcrypt.compare('somepassword', hash);
+    let cmp2 = await bcrypt.compare('somepasswor2', hash);
+    res.json({
+        body: req.body,
+        pass: generatePassword(10),
+        cmp1: cmp1,
+        cmp2: cmp2
+    });
+}));
 module.exports = router;
--- server/routes/auth/signup.js
+++ server/routes/auth/signup.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const asyncMiddleware = require('../../middlewares/async');
+const bodyValidationMiddleware = require('../../middlewares/validation').bodyValidation;
+const validationSchema = require('../../validations/signup-teams').signupSchema;
+const teamCollection = require('../../db/team');
+
+const bcrypt = require('bcrypt');
+const generatePassword = require('../../util/passwd-gen').generatePassword;
 
 router.get('/', (req, res) => {
     res.json({b: 13})
 });
+
+router.post('/', bodyValidationMiddleware(validationSchema), asyncMiddleware( async (req, res) => {
+    let team = req.body;
+    let password = generatePassword(10);
+    console.log('Password :', password);
+    team.password = await bcrypt.hash(password, 10);
+    let result = await teamCollection.newTeam(team);
+    res.json({result: result});
+}));
 module.exports = router;
