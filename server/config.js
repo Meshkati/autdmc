5c448b55efa9939d0286b61cbf61bba758ed7684
Ahmad Anvari <ahmad.anv@gmail.com>
2018-11-23 22:50:20 +0330

    Fix bug in port name

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
     PORT: process.env.PORT || '3000',
 
-    DB_HOST: process.env.DB_HOST || 'localhost',
-    DB_PORT: process.env.DB_HOST || '27017',
+    DB_HOST: process.env.DB_HOST || '172.17.0.2',
+    DB_PORT: process.env.DB_PORT || '27017',
     DB_NAME: process.env.DB_NAME || 'autdmc'
 };
