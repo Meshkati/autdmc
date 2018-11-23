d3a8ab4d7d4d84082f7e7638d87d93ae2ada2989
Ahmad Anvari <ahmad.anv@gmail.com>
2018-11-23 13:57:29 +0330

    Add db connection and body parser

--- server/server.js
+++ server/server.js
@@ -3,14 +3,31 @@ const http = require('http');
 const app = express();
 const port = require('./config').PORT;
 const route = require('./route');
+const db = require('./db/db');
+var bodyParser = require('body-parser');
+
+// configure the app to use bodyParser()
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 app.use('/apiv2', route);
 
 app.set('port', port);
 
 const server = http.createServer(app);
-server.listen(port);
-server.on('listening', onListening);
+
+
+
+db.connect().   // Try to connect to db before initializing http server
+then(
+    () => {
+    server.listen(port);
+    server.on('listening', onListening);
+}
+);
+
 
 function onListening() {
     let addr = server.address();
