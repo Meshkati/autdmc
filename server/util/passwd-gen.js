a1c233fa21ec873f018ae55be47b7d154af789b5
Ahmad Anvari <ahmad.anv@gmail.com>
2018-11-23 20:23:10 +0330

    Add password generator

---
+++ server/util/passwd-gen.js
@@ -0,0 +1,9 @@
+module.exports.generatePassword = (len) => {
+    let text = "";
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < len; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+};
